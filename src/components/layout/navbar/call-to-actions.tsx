1d247f7db9d7b20441a0aef8809e641a1b3df4e2
chourafiDev <chourafidev@gmail.com>
2025-10-18 18:39:27 +0100

    fix: change variant

--- src/components/layout/navbar/call-to-actions.tsx
+++ src/components/layout/navbar/call-to-actions.tsx
@@ -4,7 +4,7 @@ import { TiHeartFullOutline } from "react-icons/ti";
 const CallToActions = () => {
 	return (
 		<div className="flex items-center gap-2.5">
-			<Button variant={"secondary"}>Sign In</Button>
+			<Button variant={"default"}>Sign In</Button>
 			<Button>
 				<TiHeartFullOutline />
 				Donate
